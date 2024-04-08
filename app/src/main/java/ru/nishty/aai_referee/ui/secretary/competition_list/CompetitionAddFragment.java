b9c74b6b5b06d9faa94ad2f9d66ed0835f99afb7
Nishtyachok <mr.roma.roman@inbox.ru>
2024-04-09 01:15:03 +0700

    ui_secretary (open)

--- app/src/main/java/ru/nishty/aai_referee/ui/secretary/competition_list/CompetitionAddFragment.java
+++ app/src/main/java/ru/nishty/aai_referee/ui/secretary/competition_list/CompetitionAddFragment.java
@@ -1,26 +1,41 @@
 package ru.nishty.aai_referee.ui.secretary.competition_list;
 
+import android.app.DatePickerDialog;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+
+import java.util.Calendar;
+import java.util.List;
+import java.util.Locale;
+import java.util.UUID;
+import java.util.concurrent.atomic.AtomicBoolean;
+
 import ru.nishty.aai_referee.R;
 import ru.nishty.aai_referee.db.secretary.DataBaseHelperSecretary;
+import ru.nishty.aai_referee.entity.secretary.Category;
 import ru.nishty.aai_referee.entity.secretary.CompetitionSecretary;
-
-import java.util.UUID;
+import ru.nishty.aai_referee.entity.secretary.Judge;
+import ru.nishty.aai_referee.entity.secretary.Player;
+import ru.nishty.aai_referee.entity.secretary.Region;
 
 public class CompetitionAddFragment extends Fragment {
 
     private EditText etCompetitionName;
     private EditText etCompetitionDate;
+    private EditText etCompetitionDate2;
     private EditText etCompetitionLocation;
     private EditText etHeadJudge;
     private EditText etHeadSecretary;
@@ -40,10 +55,15 @@ public class CompetitionAddFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
 
         etCompetitionName = view.findViewById(R.id.etCompetitionName);
+        etCompetitionDate2 = view.findViewById(R.id.etCompetitionDate2);
         etCompetitionDate = view.findViewById(R.id.etCompetitionDate);
         etCompetitionLocation = view.findViewById(R.id.etCompetitionLocation);
         etHeadJudge = view.findViewById(R.id.etHeadJudge);
         etHeadSecretary = view.findViewById(R.id.etHeadSecretary);
+        AtomicBoolean isDatePickerOpened2 = new AtomicBoolean(false);
+        AtomicBoolean isDatePickerOpened = new AtomicBoolean(false);
+
+
 
         dataBaseHelperSecretary = new DataBaseHelperSecretary(getContext());
         SQLiteDatabase dbr = dataBaseHelperSecretary.getWritableDatabase();
@@ -52,6 +72,107 @@ public class CompetitionAddFragment extends Fragment {
         // Создание соревнования при создании фрагмента
         competitionSecretary = new CompetitionSecretary();
         competitionSecretary.setUuid(uuid);
+
+        etCompetitionDate2.setOnClickListener(v -> {
+            // Получаем текущую дату
+            Calendar calendar = Calendar.getInstance();
+            int year = calendar.get(Calendar.YEAR);
+            int month = calendar.get(Calendar.MONTH);
+            int dayOfMonth = calendar.get(Calendar.DAY_OF_MONTH);
+
+            // Открываем диалоговое окно выбора даты
+            DatePickerDialog datePickerDialog = new DatePickerDialog(getActivity(),
+                    (view1, selectedYear, selectedMonth, selectedDayOfMonth) -> {
+                        // Форматируем выбранную дату
+                        String selectedDate = String.format(Locale.getDefault(), "%02d.%02d.%d", selectedDayOfMonth, selectedMonth + 1, selectedYear);
+                        // Устанавливаем выбранную дату в EditText
+                        etCompetitionDate2.setText(selectedDate);
+                    }, year, month, dayOfMonth);
+
+            // Показываем диалоговое окно выбора даты
+            datePickerDialog.show();
+        });
+
+// Отключаем свойство фокусировки для etDate EditText
+        etCompetitionDate2.setFocusable(false);
+        etCompetitionDate2.setClickable(true); // Разрешаем нажатия на etDate EditText
+
+        etCompetitionDate2.setOnFocusChangeListener((v, hasFocus) -> {
+            // Open Date Picker Dialog only if it has not been opened before
+            if (hasFocus) {
+                // Get current date
+                Calendar calendar = Calendar.getInstance();
+                int year = calendar.get(Calendar.YEAR);
+                int month = calendar.get(Calendar.MONTH);
+                int dayOfMonth = calendar.get(Calendar.DAY_OF_MONTH);
+
+                // Launch Date Picker Dialog
+                DatePickerDialog datePickerDialog = new DatePickerDialog(getActivity(),
+                        (view1, selectedYear, selectedMonth, selectedDayOfMonth) -> {
+                            // Format the selected date
+                            String selectedDate = String.format(Locale.getDefault(), "%02d-%02d-%d", selectedDayOfMonth, selectedMonth + 1, selectedYear);
+                            // Set the selected date to the EditText
+                            etCompetitionDate2.setText(selectedDate);
+                        }, year, month, dayOfMonth);
+
+                // Show the Date Picker Dialog
+                datePickerDialog.show();
+
+                // Set the flag to true indicating the Date Picker Dialog has been opened
+                isDatePickerOpened2.set(true);
+            }
+        });
+
+        etCompetitionDate.setOnClickListener(v -> {
+            Toast.makeText(getActivity(), "Если соревнование в 1 день - не указывайте дату окончания", Toast.LENGTH_SHORT).show();
+            // Получаем текущую дату
+            Calendar calendar = Calendar.getInstance();
+            int year = calendar.get(Calendar.YEAR);
+            int month = calendar.get(Calendar.MONTH);
+            int dayOfMonth = calendar.get(Calendar.DAY_OF_MONTH);
+
+            // Открываем диалоговое окно выбора даты
+            DatePickerDialog datePickerDialog = new DatePickerDialog(getActivity(),
+                    (view1, selectedYear, selectedMonth, selectedDayOfMonth) -> {
+                        // Форматируем выбранную дату
+                        String selectedDate = String.format(Locale.getDefault(), "%02d.%02d.%d", selectedDayOfMonth, selectedMonth + 1, selectedYear);
+                        // Устанавливаем выбранную дату в EditText
+                        etCompetitionDate.setText(selectedDate);
+                    }, year, month, dayOfMonth);
+
+            // Показываем диалоговое окно выбора даты
+            datePickerDialog.show();
+        });
+
+// Отключаем свойство фокусировки для etDate EditText
+        etCompetitionDate.setFocusable(false);
+        etCompetitionDate.setClickable(true); // Разрешаем нажатия на etDate EditText
+
+        etCompetitionDate.setOnFocusChangeListener((v, hasFocus) -> {
+            // Open Date Picker Dialog only if it has not been opened before
+            if (hasFocus) {
+                // Get current date
+                Calendar calendar = Calendar.getInstance();
+                int year = calendar.get(Calendar.YEAR);
+                int month = calendar.get(Calendar.MONTH);
+                int dayOfMonth = calendar.get(Calendar.DAY_OF_MONTH);
+
+                // Launch Date Picker Dialog
+                DatePickerDialog datePickerDialog = new DatePickerDialog(getActivity(),
+                        (view1, selectedYear, selectedMonth, selectedDayOfMonth) -> {
+                            // Format the selected date
+                            String selectedDate = String.format(Locale.getDefault(), "%02d-%02d-%d", selectedDayOfMonth, selectedMonth + 1, selectedYear);
+                            // Set the selected date to the EditText
+                            etCompetitionDate.setText(selectedDate);
+                        }, year, month, dayOfMonth);
+
+                // Show the Date Picker Dialog
+                datePickerDialog.show();
+
+                // Set the flag to true indicating the Date Picker Dialog has been opened
+                isDatePickerOpened.set(true);
+            }
+        });
 
         Button addCompetitionButton = view.findViewById(R.id.btnCreateCompetition);
         addCompetitionButton.setOnClickListener(v -> {
@@ -69,18 +190,75 @@ public class CompetitionAddFragment extends Fragment {
 
 
     private void saveCompetitionToDatabase(SQLiteDatabase dbr) {
-        competitionSecretary.setName(etCompetitionName.getText().toString().trim());
-        competitionSecretary.setYear(etCompetitionDate.getText().toString().trim());
-        competitionSecretary.setPlace(etCompetitionLocation.getText().toString().trim());
-        competitionSecretary.setHeadJudge(etHeadJudge.getText().toString().trim());
-        competitionSecretary.setHeadSecretary(etHeadSecretary.getText().toString().trim());
+        String competitionName = etCompetitionName.getText().toString().trim();
+        String ctd = etCompetitionDate.getText().toString().trim();
+        String ctd2 = etCompetitionDate2.getText().toString().trim();
+        String competitionDate = (!ctd.equals(""))?(ctd2 + " - " + ctd):(ctd2);
+        String competitionLocation = etCompetitionLocation.getText().toString().trim();
+        String headJudge = etHeadJudge.getText().toString().trim();
+        String headSecretary = etHeadSecretary.getText().toString().trim();
+
+        List<Region> regions = dataBaseHelperSecretary.getRegions(dbr, uuid);
+        List<Category> categories = dataBaseHelperSecretary.getCategories(dbr, uuid);
+        List<Player> players = dataBaseHelperSecretary.getPlayers(dbr, uuid);
+        List<Judge> judgs = dataBaseHelperSecretary.getJudges(dbr, uuid);
+
+        if (TextUtils.isEmpty(competitionName)) {
+            Toast.makeText(getActivity(), "Введите название соревнования", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (TextUtils.isEmpty(competitionDate)) {
+            Toast.makeText(getActivity(), "Введите дату соревнования", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+
+
+        if (TextUtils.isEmpty(competitionLocation)) {
+            Toast.makeText(getActivity(), "Введите место проведения соревнования", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (TextUtils.isEmpty(headJudge)) {
+            Toast.makeText(getActivity(), "Введите главного судью", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (TextUtils.isEmpty(headSecretary)) {
+            Toast.makeText(getActivity(), "Введите главного секретаря", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (categories.size() == 0) {
+            Toast.makeText(getActivity(), "Добавьте категории в соревнование", Toast.LENGTH_SHORT).show();
+            return;
+        } else if (regions.size() == 0) {
+            Toast.makeText(getActivity(), "Добавьте регионы в соревнование", Toast.LENGTH_SHORT).show();
+            return;
+        } else if (players.size() == 0) {
+            Toast.makeText(getActivity(), "Добавьте игроков в соревнование", Toast.LENGTH_SHORT).show();
+            return;
+        } else if (judgs.size() == 0) {
+            Toast.makeText(getActivity(), "Добавьте судей в соревнование", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+
+        // Установка данных соревнования
+        competitionSecretary.setName(competitionName);
+        competitionSecretary.setYear(competitionDate);
+        competitionSecretary.setPlace(competitionLocation);
+        competitionSecretary.setHeadJudge(headJudge);
+        competitionSecretary.setHeadSecretary(headSecretary);
+
 
         // Сохранение соревнования в базе данных
         dataBaseHelperSecretary.addCompetition(dbr, competitionSecretary);
 
-        // Очистка полей ввода после сохранения
-
+        NavHostFragment.findNavController(this)
+                .navigateUp();
     }
+
+
 
     private void navigateToJudgesFragment() {
         Bundle bundle = new Bundle();
@@ -97,6 +275,7 @@ public class CompetitionAddFragment extends Fragment {
     private void clearInputFields() {
         etCompetitionName.setText("");
         etCompetitionDate.setText("");
+        etCompetitionDate2.setText("");
         etCompetitionLocation.setText("");
         etHeadJudge.setText("");
         etHeadSecretary.setText("");
--- app/src/main/java/ru/nishty/aai_referee/ui/secretary/performance_list/PerformanceAddFragment.java
+++ app/src/main/java/ru/nishty/aai_referee/ui/secretary/performance_list/PerformanceAddFragment.java
@@ -1,5 +1,7 @@
 package ru.nishty.aai_referee.ui.secretary.performance_list;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Color;
 import android.os.Bundle;
@@ -17,8 +19,11 @@ import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
+import java.util.Locale;
 import java.util.UUID;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import ru.nishty.aai_referee.R;
 import ru.nishty.aai_referee.db.secretary.DataBaseHelperSecretary;
@@ -57,6 +62,8 @@ public class PerformanceAddFragment extends Fragment {
         EditText etCourtNumber = view.findViewById(R.id.etCourtNumber);
         Spinner spinnerJudge = view.findViewById(R.id.spinnerJudge);
         Button btnCreateGame = view.findViewById(R.id.btnCreateGame);
+        AtomicBoolean isTimePickerOpened = new AtomicBoolean(false);
+        AtomicBoolean isDatePickerOpened = new AtomicBoolean(false);
 
         List<Player> player1 = dataBaseHelperSecretary.getPlayers(dataBaseHelperSecretary.getWritableDatabase(), UUID.fromString(String.valueOf(competitionId)));
         List<String> player1Names = new ArrayList<>();
@@ -133,6 +140,108 @@ public class PerformanceAddFragment extends Fragment {
         spinnerPlayer1.setAdapter(player1Adapter);
         spinnerPlayer2.setAdapter(player2Adapter);
         spinnerJudge.setAdapter(judgeAdapter);
+        // Установка OnClickListener для etDate EditText
+        etDate.setOnClickListener(v -> {
+            // Получаем текущую дату
+            Calendar calendar = Calendar.getInstance();
+            int year = calendar.get(Calendar.YEAR);
+            int month = calendar.get(Calendar.MONTH);
+            int dayOfMonth = calendar.get(Calendar.DAY_OF_MONTH);
+
+            // Открываем диалоговое окно выбора даты
+            DatePickerDialog datePickerDialog = new DatePickerDialog(getActivity(),
+                    (view1, selectedYear, selectedMonth, selectedDayOfMonth) -> {
+                        // Форматируем выбранную дату
+                        String selectedDate = String.format(Locale.getDefault(), "%02d.%02d.%d", selectedDayOfMonth, selectedMonth + 1, selectedYear);
+                        // Устанавливаем выбранную дату в EditText
+                        etDate.setText(selectedDate);
+                    }, year, month, dayOfMonth);
+
+            // Показываем диалоговое окно выбора даты
+            datePickerDialog.show();
+        });
+
+// Отключаем свойство фокусировки для etDate EditText
+        etDate.setFocusable(false);
+        etDate.setClickable(true); // Разрешаем нажатия на etDate EditText
+
+        etDate.setOnFocusChangeListener((v, hasFocus) -> {
+            // Open Date Picker Dialog only if it has not been opened before
+            if (hasFocus) {
+                // Get current date
+                Calendar calendar = Calendar.getInstance();
+                int year = calendar.get(Calendar.YEAR);
+                int month = calendar.get(Calendar.MONTH);
+                int dayOfMonth = calendar.get(Calendar.DAY_OF_MONTH);
+
+                // Launch Date Picker Dialog
+                DatePickerDialog datePickerDialog = new DatePickerDialog(getActivity(),
+                        (view1, selectedYear, selectedMonth, selectedDayOfMonth) -> {
+                            // Format the selected date
+                            String selectedDate = String.format(Locale.getDefault(), "%02d-%02d-%d", selectedDayOfMonth, selectedMonth + 1, selectedYear);
+                            // Set the selected date to the EditText
+                            etDate.setText(selectedDate);
+                        }, year, month, dayOfMonth);
+
+                // Show the Date Picker Dialog
+                datePickerDialog.show();
+
+                // Set the flag to true indicating the Date Picker Dialog has been opened
+                isDatePickerOpened.set(true);
+            }
+        });
+
+        // Установка OnClickListener для etTime EditText
+        etTime.setOnClickListener(v -> {
+            // Получаем текущее время
+            Calendar calendar = Calendar.getInstance();
+            int hour = calendar.get(Calendar.HOUR_OF_DAY);
+            int minute = calendar.get(Calendar.MINUTE);
+
+            // Открываем диалоговое окно выбора времени
+            TimePickerDialog timePickerDialog = new TimePickerDialog(getActivity(),
+                    (view1, selectedHour, selectedMinute) -> {
+                        // Форматируем выбранное время
+                        String selectedTime = String.format(Locale.getDefault(), "%02d:%02d", selectedHour, selectedMinute);
+                        // Устанавливаем выбранное время в EditText
+                        etTime.setText(selectedTime);
+                    }, hour, minute, true);
+
+            // Показываем диалоговое окно выбора времени
+            timePickerDialog.show();
+        });
+
+// Отключаем свойство фокусировки для etTime EditText
+        etTime.setFocusable(false);
+        etTime.setClickable(true); // Разрешаем нажатия на etTime EditText
+
+
+        // Set OnFocusChangeListener to etTime EditText
+        etTime.setOnFocusChangeListener((v, hasFocus) -> {
+            // Open Time Picker Dialog only if it has not been opened before
+            if (hasFocus) {
+                // Get current time
+                Calendar calendar = Calendar.getInstance();
+                int hour = calendar.get(Calendar.HOUR_OF_DAY);
+                int minute = calendar.get(Calendar.MINUTE);
+
+                // Launch Time Picker Dialog
+                TimePickerDialog timePickerDialog = new TimePickerDialog(getActivity(),
+                        (view1, selectedHour, selectedMinute) -> {
+                            // Format the selected time
+                            String selectedTime = String.format(Locale.getDefault(), "%02d:%02d", selectedHour, selectedMinute);
+                            // Set the selected time to the EditText
+                            etTime.setText(selectedTime);
+                        }, hour, minute, true);
+
+                // Show the Time Picker Dialog
+                timePickerDialog.show();
+
+                // Set the flag to true indicating the Time Picker Dialog has been opened
+                isTimePickerOpened.set(true);
+            }
+        });
+
 
         btnCreateGame.setOnClickListener(v -> {
             // Получение данных из элементов управления
@@ -144,60 +253,63 @@ public class PerformanceAddFragment extends Fragment {
             String courtNumber = etCourtNumber.getText().toString();
             int selectedPositionJudge = spinnerJudge.getSelectedItemPosition() - 1;
 
-                    if (selectedPositionPlayer1 == -1) {
-                        Toast.makeText(getActivity(), "Пожалуйста, выберите первого игрока", Toast.LENGTH_SHORT).show();
-                        return;
-                    } else if (selectedPositionPlayer2 == -1) {
-                        Toast.makeText(getActivity(), "Пожалуйста, выберите второго игрока", Toast.LENGTH_SHORT).show();
-                        return;
-                    } else if (date.isEmpty()) {
-                        Toast.makeText(getActivity(), "Пожалуйста, введите дату", Toast.LENGTH_SHORT).show();
-                        return;
-                    } else if (time.isEmpty()) {
-                        Toast.makeText(getActivity(), "Пожалуйста, введите время", Toast.LENGTH_SHORT).show();
-                        return;
-                    } else if (location.isEmpty()) {
-                        Toast.makeText(getActivity(), "Пожалуйста, укажите место проведения", Toast.LENGTH_SHORT).show();
-                        return;
-                    } else if (courtNumber.isEmpty()) {
-                        Toast.makeText(getActivity(), "Пожалуйста, укажите номер площадки", Toast.LENGTH_SHORT).show();
-                        return;
-                    } else if (selectedPositionJudge == -1) {
-                        Toast.makeText(getActivity(), "Пожалуйста, выберите судью", Toast.LENGTH_SHORT).show();
-                        return;
-                    } else {
+            if (selectedPositionPlayer1 == -1) {
+                Toast.makeText(getActivity(), "Пожалуйста, выберите первого игрока", Toast.LENGTH_SHORT).show();
+                return;
+            } else if (selectedPositionPlayer2 == -1) {
+                Toast.makeText(getActivity(), "Пожалуйста, выберите второго игрока", Toast.LENGTH_SHORT).show();
+                return;
+            } else if (date.isEmpty()) {
+                Toast.makeText(getActivity(), "Пожалуйста, введите дату", Toast.LENGTH_SHORT).show();
+                return;
+            } else if (time.isEmpty()) {
+                Toast.makeText(getActivity(), "Пожалуйста, введите время", Toast.LENGTH_SHORT).show();
+                return;
+            } else if (location.isEmpty()) {
+                Toast.makeText(getActivity(), "Пожалуйста, укажите место проведения", Toast.LENGTH_SHORT).show();
+                return;
+            } else if (courtNumber.isEmpty()) {
+                Toast.makeText(getActivity(), "Пожалуйста, укажите номер площадки", Toast.LENGTH_SHORT).show();
+                return;
+            } else if (selectedPositionPlayer2 == selectedPositionPlayer1) {
+                Toast.makeText(getActivity(), "Пожалуйста, выберите разных игроков", Toast.LENGTH_SHORT).show();
+                return;
+            } else if (selectedPositionJudge == -1) {
+                Toast.makeText(getActivity(), "Пожалуйста, выберите судью", Toast.LENGTH_SHORT).show();
+                return;
+            } else {
 
-                        int selectedPlayer1 = player1Ids.get(selectedPositionPlayer1);
-                        int selectedPlayer2 = player2Ids.get(selectedPositionPlayer2);
-                        int selectedJudge = judgeIds.get(selectedPositionJudge);
+                int selectedPlayer1 = player1Ids.get(selectedPositionPlayer1);
+                int selectedPlayer2 = player2Ids.get(selectedPositionPlayer2);
+                int selectedJudge = judgeIds.get(selectedPositionJudge);
 
-                        Player playerOne = dataBaseHelperSecretary.getPlayerById(dbr,UUID.fromString(String.valueOf(competitionId)),selectedPlayer1);
-                        Player playerTwo = dataBaseHelperSecretary.getPlayerById(dbr,UUID.fromString(String.valueOf(competitionId)),selectedPlayer2);
+                Player playerOne = dataBaseHelperSecretary.getPlayerById(dbr, UUID.fromString(String.valueOf(competitionId)), selectedPlayer1);
+                Player playerTwo = dataBaseHelperSecretary.getPlayerById(dbr, UUID.fromString(String.valueOf(competitionId)), selectedPlayer2);
 
-                        List<Player> players = new ArrayList<>();
-                        if (playerOne != null && playerTwo != null) {
-                            players.add(playerOne);
-                            players.add(playerTwo);
-                        } else {
-                            // Если объекты игроков не найдены, выводим сообщение об ошибке
-                            Toast.makeText(getActivity(), "Ошибка при получении данных об игроках", Toast.LENGTH_SHORT).show();
-                            return;
-                        }
-                        PerformanceSecretary performanceSecretary = new PerformanceSecretary();
-                        performanceSecretary.setComp_id(competitionId);
-                        performanceSecretary.setPlayers(players);
-                        performanceSecretary.setDate(date);
-                        performanceSecretary.setTime(time);
-                        performanceSecretary.setPlace(location);
-                        performanceSecretary.setPlayground(courtNumber);
-                        performanceSecretary.setJudgeId(selectedJudge);
+                List<Player> players = new ArrayList<>();
+                if (playerOne != null && playerTwo != null) {
+                    players.add(playerOne);
+                    players.add(playerTwo);
+                } else {
+                    // Если объекты игроков не найдены, выводим сообщение об ошибке
+                    Toast.makeText(getActivity(), "Ошибка при получении данных об игроках", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                PerformanceSecretary performanceSecretary = new PerformanceSecretary();
+                performanceSecretary.setComp_id(competitionId);
+                performanceSecretary.setPlayers(players);
+                performanceSecretary.setDate(date);
+                performanceSecretary.setTime(time);
+                performanceSecretary.setPlace(location);
+                performanceSecretary.setPlayground(courtNumber);
+                performanceSecretary.setJudgeId(selectedJudge);
 
-                        SQLiteDatabase db = dataBaseHelperSecretary.getWritableDatabase();
-                        dataBaseHelperSecretary.addPerformance(db,performanceSecretary,players);
-                    }
+                SQLiteDatabase db = dataBaseHelperSecretary.getWritableDatabase();
+                dataBaseHelperSecretary.addPerformance(db, performanceSecretary, players);
+            }
 
-                    NavHostFragment.findNavController(this)
-                        .navigateUp();
+            NavHostFragment.findNavController(this)
+                    .navigateUp();
         });
 
         return view;
